1c281e69672d8b8722838e3a28c61524cf164928
Clyke02 <mrizkyfirdaus2203@gmail.com>
2024-12-20 21:57:24 +0700

    feat: merchant register done

--- resources/views/merchant/components/formRegisterMerchant.blade.php
+++ resources/views/merchant/components/formRegisterMerchant.blade.php
@@ -7,8 +7,8 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-300">
-    <div class="w-full md:w-2/3 bg-gray-300 flex items-center justify-center py-10">
-        <div class="w-full max-w-2xl bg-white p-12 rounded-2xl shadow-lg overflow-y-auto max-h-screen">
+    <div class="w-full md:w-2/3 bg-gray-300 flex items-center justify-center">
+        <div class="w-full max-w bg-white p-12 rounded-2xl shadow-lg overflow-y-auto max-h-screen">
             <div class="text-center mb-8">
                 <h1 class="text-3xl font-bold mb-2 text-gray-800">Daftar Kemitraan</h1>
                 <p class="text-gray-600">
