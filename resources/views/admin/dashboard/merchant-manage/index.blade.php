a67da6570e95ed5b3a0954d08bbc9e64b83bda50
Reyn12 <muhammadrenaldi687@gmail.com>
2024-12-22 21:40:36 +0700

    feat: tambah card all merchant di merchant-manage

--- resources/views/admin/dashboard/merchant-manage/index.blade.php
+++ resources/views/admin/dashboard/merchant-manage/index.blade.php
@@ -12,34 +12,42 @@
             font-family: 'Lato', sans-serif;
         }
         .bg-primary {
-        background-color: #0039C9;
+            background-color: #0039C9;
         }
 
         .text-primary {
             color: #0039C9;
         }
     </style>
-
 </head>
 <body>
-    
     {{-- container --}}
     <div class="w-full h-screen flex bg-gray-100">
-        
         {{-- Include Sidebar Component --}}
         @include('admin.components.sidebar')
 
         {{-- Content --}}
         <div class="content flex-1 h-full p-4 overflow-y-scroll">
-            
             {{-- Header --}}
             <div class="sticky top-0 z-10 mb-4">
                 @include('admin.components.header')
             </div>
             
             {{-- Main Content --}}
-            <div class="flex gap-3">
-                
+            <div class="flex flex-col bg-white rounded-lg shadow-lg"> 
+                {{-- Navigation Tabs --}}
+                @include('admin.components.nav-MerchantManage')
+
+                {{-- Sort dan Filter Merchant--}}
+                @include('admin.components.sortDanFilterMerchant')
+
+                {{-- Card All Merchant --}}
+                @include('admin.components.cardAllMerchant')
+
+                {{-- Content Area (kosong dulu) --}}
+                <div class="p-6">
+                    <!-- Content akan ditambahkan nanti -->
+                </div>
             </div>
         </div>
     </div>
