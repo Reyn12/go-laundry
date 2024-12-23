111b8268ce6e7c31b17d55a0685744c677dfe886
HAMIDUMMAZIDUN <hamidabdulaziz36@gmail.com>
2024-12-23 17:59:34 +0700

    feat: fix file web.php

--- routes/web.php
+++ routes/web.php
@@ -13,35 +13,20 @@ use App\Http\Controllers\MerchantController\DashboardMerchantController;
 use App\Http\Controllers\MerchantController\LoginMerchantController;
 use App\Http\Controllers\AdminController\UserManageController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// Homepage route
 Route::get('/', function () {
     return view('homepage.index');
-});
+})->name('homepage');
 
 // Admin Routes
 Route::prefix('admin')->group(function () {
     Route::get('/login', function () {
         return view('admin.login.index');
-    });
+    })->name('admin.login');
     Route::post('/login', [LoginController::class, 'login']);
-    
-    // Dashboard route
-    Route::get('/dashboard', [DashboardAdminController::class, 'index'])->name('admin.dashboard');
 
-    // User Management route
+    Route::get('/dashboard', [DashboardAdminController::class, 'index'])->name('admin.dashboard');
     Route::get('/dashboard/user-manage', [UserManageController::class, 'index'])->name('admin.dashboard.user-manage.index');
-    
-    //Admin register 
     Route::get('/register', [RegisterController::class, 'index'])->name('admin.register');
 });
 
@@ -49,32 +34,21 @@ Route::prefix('admin')->group(function () {
 Route::prefix('user')->group(function () {
     Route::get('/login', function () {
         return view('user.login.index');
-    });
+    })->name('user.login');
     Route::post('/login', [LoginUserController::class, 'login']);
-    
-    // Dashboard route
-    Route::get('/dashboard', [DashboardUserController::class, 'index'])->name('user.dashboard');
-    
-    //Admin register 
-    Route::get('/register', [RegisterUserController::class, 'index'])->name('user.register');
 
-     //User Search Route
-     Route::get('/pencarian', [UserPencarianController::class, 'index'])->name('user.pencarian');
+    Route::get('/dashboard', [DashboardUserController::class, 'index'])->name('user.dashboard');
+    Route::get('/pencarian', [UserPencarianController::class, 'index'])->name('user.pencarian');
+    Route::get('/register', [RegisterUserController::class, 'index'])->name('user.register');
 });
 
-    // Merchant Routes
-    Route::prefix('merchant')->group(function () {
+// Merchant Routes
+Route::prefix('merchant')->group(function () {
     Route::get('/login', function () {
         return view('merchant.login.index');
-    });
+    })->name('merchant.login');
     Route::post('/login', [LoginMerchantController::class, 'login']);
-    
-    // Dashboard route
+
     Route::get('/dashboard', [DashboardMerchantController::class, 'index'])->name('merchant.dashboard');
-    
-    //Admin register 
     Route::get('/register', [RegisterMerchantController::class, 'index'])->name('merchant.register');
-    });
-
-
-
+});
